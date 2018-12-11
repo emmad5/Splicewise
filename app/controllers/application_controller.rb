f681b62c50d165568ed99d583ddc4d6679e73531
Emma Deas <emmadeas9@gmail.com>
2018-12-11 13:40:07 -0800

    add backend user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+        @current_user = user
+    end
+
+    def logout!
+        if current_user
+            current_user.reset_session_token!
+        end
+        @current_user = nil
+        session[:session_token] = nil
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { base: ['Invalid Credentials!'] }, status: 401
+        end
+    end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+    attr_reader :password
+    validates :username, :password_digest, :session_token, presence: true
+    validates :useranme, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    before_validation :ensure_session_token
+
+    def password=(password) 
+        @password = password
+        self.password_digest = BCrypyt::Password.create(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user
+        end
+        nil
+    end
+
+    def is_password?(password)
+        BCrypyt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    private
+    
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+end
