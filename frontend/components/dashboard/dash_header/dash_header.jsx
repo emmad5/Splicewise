4947a39c8623f7908f0794a1ab63286aef36d0e6
Emma Deas <emmadeas9@gmail.com>
2019-02-01 09:58:26 -0800

    remove extra link

--- frontend/components/dashboard/dash_header/dash_header.jsx
+++ frontend/components/dashboard/dash_header/dash_header.jsx
@@ -30,7 +30,7 @@ const DashHeader = ({openModal, friend, payments, currentUser}) => {
                <h1 className="dashboardtitle">{dashboard}</h1>
                <div className="dashbuttons">
                     <button onClick={() => openModal('billform')} className='dash-button'>Add A Bill</button>
-                    <a href="https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/05/12/kittens.jpg?w968h681" target="_blank" className='dash-settle-button'>Settle Up</a>
+                    <a className='dash-settle-button'>Settle Up</a>
                 </div>
             </nav>
             <div className='dashbottom'>
